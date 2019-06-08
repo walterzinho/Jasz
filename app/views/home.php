9645f0f7ae9cfa63ddce3994f5d24cc3340af196
Walter Jairzinho Sosa <waltersosa@gmail.com>
2019-06-07 20:04:49 -0500

    Add files via upload

---
+++ app/views/home.php
@@ -0,0 +1,1 @@
+<h2>Inicio - JASZ Comunicaciones</h2>
---
+++ app/views/home_metas.php
@@ -0,0 +1,5 @@
+<?php
+$meta_title =  'Inicio - JASZ Comunicaciones';
+$meta_description = 'Descripción del Home.';
+$meta_img = './img/meta-home.jpg';
+$meta_url = $_SERVER["HTTP_HOST"] .  $_SERVER["REQUEST_URI"];
